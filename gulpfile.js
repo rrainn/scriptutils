b299ef738e98d2404bdb683abe279e1714e10ff6
Charlie Fish <fishcharlie@me.com>
2017-11-11 00:30:49 -0700

    Adding support for babel plugins in browser build

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,28 @@
 var gulp = require('gulp');
-var browserify = require('gulp-browserify');
+var browserify = require('browserify');
 var babel = require('gulp-babel');
+var babelify = require('babelify');
 var uglify = require('gulp-uglify');
 var rename = require("gulp-rename");
+var buffer = require('vinyl-buffer');
+var source = require('vinyl-source-stream');
 
 
-gulp.task('default', function() {
-    // Browser Version
-    gulp.src("./src/index.js")
-        .pipe(browserify({
-          insertGlobals : true,
-          debug : false
-        }))
-        .pipe(babel())
-        .pipe(uglify())
-        .pipe(rename("index.js"))
-        .pipe(gulp.dest("./dist/browser"))
-        
-    // Node version
-    gulp.src("./src/**")
-		.pipe(babel())
-        .pipe(gulp.dest("./dist/node"))
+gulp.task('default', ["browser", "node"]);
+
+gulp.task('browser', function () {
+    return browserify({entries: './index.js', extensions: ['.js'], debug: true, basedir: __dirname + '/src'})
+      .transform(babelify)
+      .bundle()
+      .pipe(source('index.js'))
+      .pipe(buffer())
+      .pipe(uglify())
+      .pipe(rename("index.js"))
+      .pipe(gulp.dest("./dist/browser"))
 });
+
+gulp.task('node', function () {
+    gulp.src("./src/**")
+        .pipe(babel())
+        .pipe(gulp.dest("./dist/node"))
+})
