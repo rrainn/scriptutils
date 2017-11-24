46fdc9cbda1bb250f416d9d058d6f740fe218a50
Charlie Fish <fishcharlie@me.com>
2017-11-24 01:22:15 -0700

    Fixing IE 9 Test

--- src/string/index.js
+++ src/string/index.js
@@ -19,5 +19,24 @@ String.prototype.removeBefore = function (character) {
 			self = self.substring((self.lastIndexOf(element[element.length-1]) >= 0 && element.length > 0) ? self.lastIndexOf(element[element.length-1])+1 : 0);
 		}
 	});
+	self = IEObjectToString(self);
 	return self;
 };
+
+
+// IE 9 fix where String.removeAfter and String.removeBefore returned an object when passing an empty array in, this function will convert that object back to a string
+function IEObjectToString(string) {
+	/* istanbul ignore if */
+	if (typeof string == "object") {
+		string = Object.keys(string).filter(function (item) {
+			return !isNaN(parseInt(item));
+		}).sort(function (a,b) {
+			return a>b;
+		}).map(function (value) {
+			return string[value];
+		}).reduce(function (a,b) {
+			return a + b;
+		});
+	}
+	return string;
+}
