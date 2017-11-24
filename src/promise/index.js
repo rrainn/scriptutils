c576c557c2b9640c6f4ee23f8a1fe7300a701a0e
Charlie Fish <fishcharlie@me.com>
2017-11-23 23:09:17 -0700

    Adding Promise.reflect function

--- src/promise/index.js
+++ src/promise/index.js
@@ -1,3 +1,13 @@
+Promise.reflect = function (promises) {
+	return Promise.all(promises.map((promise) => {
+		return promise.then((v) => {
+			return {v, status: "fulfilled"};
+		}, (e) => {
+			return {e, status: "rejected"};
+		});
+	}));
+};
+
 Promise.prototype.state = function () {
   const promiseCheck = {};
   return Promise.race([this, promiseCheck]).then(value => (value === promiseCheck) ? "pending" : "fulfilled", () => "rejected");
