186acc8a9a222f90e1361994fcc09df68f3cd38c
Charlie Fish <fishcharlie@me.com>
2017-11-23 21:45:53 -0700

    Adding String.removeBefore function

--- src/string/index.js
+++ src/string/index.js
@@ -8,3 +8,16 @@ String.prototype.removeAfter = function (character) {
 	});
 	return self;
 };
+
+String.prototype.removeBefore = function (character) {
+	let self = this;
+	if (!Array.isArray(character)) {
+		character = [character];
+	}
+	character.forEach((element) => {
+		if (element) {
+			self = self.substring((self.lastIndexOf(element[element.length-1]) >= 0 && element.length > 0) ? self.lastIndexOf(element[element.length-1])+1 : 0);
+		}
+	});
+	return self;
+};
